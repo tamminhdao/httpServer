a3ec9aaf19def319673a754fcef59aa7a93c9c5e
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2017-12-06 16:15:53 -0600

    able to print incoming request to the console

--- Sources/Server/server.swift
+++ Sources/Server/server.swift
@@ -3,25 +3,46 @@ import Socket
 
 public class Server {
 
+    var listeningSocket: Socket!
+    var clientSocket: Socket!
+    var readData = Data()
+
     public init() {}
 
     public func run() {
-
-        var listeningSocket: Socket!
-        var clientSocket: Socket!
-
         do {
-            try listeningSocket = Socket.create()
-            try listeningSocket.listen(on: 5000)
-
+            try self.listeningSocket = Socket.create()
+            try self.listeningSocket.listen(on: 5000)
             repeat {
-                try clientSocket = listeningSocket.acceptClientConnection()
-                let response = Data("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n".utf8)
-                try clientSocket.write(from: response)
+                try clientSocket = self.listeningSocket.acceptClientConnection()
+                printRequest(socket: clientSocket)
+                response200(socket: clientSocket)
+//                clientSocket.close()
             } while true
+        } catch let error {
+            print (error.localizedDescription)
         }
-        catch {
-            print ("Something went wrong")
+    }
+
+    public func response200(socket: Socket) {
+        do {
+            let response = Data("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n".utf8)
+            try socket.write(from: response)
+        }
+        catch let error {
+            print (error.localizedDescription)
+        }
+    }
+
+    public func printRequest(socket: Socket) {
+        do {
+                _ = try socket.read(into: &readData)
+                let request = String(data: readData, encoding: .utf8)
+                let reply = "Received:\n \(request!)\n"
+                print(reply)
+                readData.count = 0
+        } catch let error {
+            print (error.localizedDescription)
         }
     }
 }
