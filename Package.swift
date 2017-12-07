0b57dff8a9e1f38a30b97a89e3b0da7d206e7bda
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2017-12-06 21:47:28 -0600

    update package to create requestParser class

--- Package.swift
+++ Package.swift
@@ -17,9 +17,15 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Main",
-            dependencies: ["Server"]),
+            dependencies: ["Server", "Requests"]),
+        .target(
+            name: "Requests",
+            dependencies: []),
         .target(
             name: "Server",
             dependencies: ["Socket"]),
+        .testTarget(
+            name: "RequestsSpec",
+            dependencies: ["Requests", "Quick", "Nimble"]),
     ]
 )
