1dc194ffd9146f4ba41f20e35822fce7d8a71d06
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2017-12-08 00:50:14 -0600

    WIP - debugging

--- Sources/Requests/requestParser.swift
+++ Sources/Requests/requestParser.swift
@@ -16,9 +16,23 @@ public class RequestParser {
             var lines = getLine(request: request)
 
             let firstLine = lines.removeFirst()
+
+        print("does it get to the first line")
+        print(firstLine)
+
             let statusLine = try parseStatusLine(statusLine: firstLine)
 
+        print("does it get to the status line")
+        print(statusLine)
+
+        print("Let's see how the array of lines looks like")
+        print(lines)
+
             let headers = parseHeaders(headerLines: lines)
+
+        print("does it get to the headers line? - NO")
+        print(headers)
+
 
             let parsedRequest = HttpRequest(
                 method: statusLine.method,
@@ -52,7 +66,7 @@ public class RequestParser {
     public func parseHeaders(headerLines: [String]) -> [String: String] {
         var headers = [String: String]()
 
-        for line in headerLines {
+        for line in headerLines where line.contains(":") {
             let keyValue = line.split(separator: ":", maxSplits: 1)
             let trimmedKey = keyValue[0].trimmingCharacters(in: .whitespacesAndNewlines)
             let trimmedValue = keyValue[1].trimmingCharacters(in: .whitespacesAndNewlines)
