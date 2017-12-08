d1f3495bb5ca76d8eba88e238b453680699137d3
Tam Dao <tamminhdaoabbas@gmail.com>
2017-12-07 23:02:19 -0600

    define equality for HttpRequest objects

--- Sources/Requests/httpRequest.swift
+++ Sources/Requests/httpRequest.swift
@@ -1,5 +1,25 @@
 import Foundation
 
 public class HttpRequest {
-    
+    var method: String
+    var url: String
+    var version: String
+    var headers: [String: String]
+
+    public init(method: String, url: String, version: String, headers: [String: String]) {
+        self.method = method
+        self.url = url
+        self.version = version
+        self.headers = headers
+    }
+}
+
+extension HttpRequest: Equatable {
+    public static func == (lhs: HttpRequest, rhs: HttpRequest) -> Bool {
+        return
+            lhs.method == rhs.method &&
+            lhs.url == rhs.url &&
+            lhs.version == rhs.version &&
+            lhs.headers == rhs.headers
+    }
 }
