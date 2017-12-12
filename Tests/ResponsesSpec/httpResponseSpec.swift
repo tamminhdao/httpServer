75578dead910fab539d48ca549eea8f6cc0d759f
Tam Dao <tamminhdaoabbas@gmail.com>
2017-12-12 17:02:06 -0600

    convert response object to data type

--- Tests/ResponsesSpec/httpResponseSpec.swift
+++ Tests/ResponsesSpec/httpResponseSpec.swift
@@ -23,6 +23,19 @@ class HttpResponseSpec: QuickSpec {
 
                 expect(response1).to(equal(response2))
             }
+
+            it ("can convert a response object to type Data") {
+                let response = HttpResponse(
+                        version: "HTTP/1.1",
+                        statusCode: 200,
+                        statusPhrase: "OK",
+                        headers: [:]
+                )
+
+                let data = response.constructResponse()
+                let expected = Data("HTTP/1.1 200 OK\r\n\r\n".utf8)
+                expect(data).to(equal(expected))
+            }
         }
     }
 }
