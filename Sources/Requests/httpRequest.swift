9581a562fdd9251b39b7cf6a4f36dbc96bcd18a4
Tam Dao <tamminhdaoabbas@gmail.com>
2017-12-12 11:45:02 -0600

    add crlf attribute

--- Sources/Requests/httpRequest.swift
+++ Sources/Requests/httpRequest.swift
@@ -5,12 +5,16 @@ public class HttpRequest {
     var url: String
     var version: String
     var headers: [String: String]
+    var crlf: String
+    var space: String
 
     public init(method: String, url: String, version: String, headers: [String: String]) {
         self.method = method
         self.url = url
         self.version = version
         self.headers = headers
+        self.crlf = "\r\n"
+        self.space = " "
     }
 
     public func returnMethod () -> String {
