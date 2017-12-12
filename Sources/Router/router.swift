f1e3f494ea005201cab9628d25b2174e155ea5df
Tam Dao <tamminhdaoabbas@gmail.com>
2017-12-12 16:37:03 -0600

    set up router

---
+++ Sources/Router/router.swift
@@ -0,0 +1,43 @@
+import Foundation
+import Requests
+import Responses
+
+public class Router {
+
+    private var routes: [String: [String]]
+
+    private func routeSetUp() {
+        routes["/"] = ["GET", "POST", "PUT"]
+        routes["/form"] = ["GET", "POST", "PUT"]
+    }
+
+    public init() {
+        self.routes = [:]
+        self.routeSetUp()
+    }
+
+    public func checkRoute(request: HttpRequest) -> HttpResponse {
+        let requestMethod = request.returnMethod()
+        let requestUrl = request.returnUrl()
+
+        for route in routes {
+            if route.key == requestUrl {
+                for method in route.value {
+                    if method == requestMethod {
+                        return generate200Response()
+                    }
+                }
+            }
+        }
+
+        return generate404Response()
+    }
+
+    private func generate200Response() -> HttpResponse {
+        return HttpResponse(version: "HTTP/1.1", statusCode: 200, statusPhrase: "OK", headers: [:])
+    }
+
+    private func generate404Response() -> HttpResponse {
+        return HttpResponse(version: "HTTP/1.1", statusCode: 404, statusPhrase: "NotFound", headers: [:])
+    }
+}
--- Tests/RouterSpec/routerSpec.swift
+++ Tests/RouterSpec/routerSpec.swift
@@ -3,41 +3,62 @@ import Quick
 import Nimble
 import Router
 import Requests
+import Responses
 
 class RouterSpec: QuickSpec {
-//    override func spec() {
-//        describe("#Router") {
-//            var router: Router!
-//
-//            beforeEach {
-//                router = Router()
-//            }
+    override func spec() {
+        describe("#Router") {
+            var router: Router!
 
-//            it ("return a 200 OK response if the method/url combo is correct") {
-//                let validRequest = HttpRequest(
-//                        method: "GET",
-//                        url: "/",
-//                        version: "HTTP/1.1",
-//                        headers: [
-//                            "Host": "localhost:5000",
-//                            "User-Agent": "curl/7.54.0",
-//                            "Accept": "*/*"
-//                        ]
-//                )
-//
-//                let responseOK = HttpResponse(
-//                        version = "HTTP/1.1",
-//                        statusCode = 200,
-//                        statusPhrase = "OK",
-//                        headers = nil
-//                )
-//                let response = router.checkRoute(request: validRequest)
-//                expect(response).to(equal(responseOK))
-//            }
+            beforeEach {
+                router = Router()
+            }
 
-//            it ("returns a 400 NotFound if the method/url combo is not correct") {
-//
-//            }
-//        }
-//    }
+            it ("return a 200 OK response if the method/url combo is correct") {
+                let validRequest = HttpRequest(
+                        method: "GET",
+                        url: "/",
+                        version: "HTTP/1.1",
+                        headers: [
+                            "Host": "localhost:5000",
+                            "User-Agent": "curl/7.54.0",
+                            "Accept": "*/*"
+                        ]
+                )
+
+                let responseOK = HttpResponse(
+                        version: "HTTP/1.1",
+                        statusCode: 200,
+                        statusPhrase: "OK",
+                        headers: [:]
+                )
+
+                let response = router.checkRoute(request: validRequest)
+                expect(response).to(equal(responseOK))
+            }
+
+            it ("returns a 400 NotFound if the method/url combo is not correct") {
+                let validRequest = HttpRequest(
+                        method: "GET",
+                        url: "/foobar",
+                        version: "HTTP/1.1",
+                        headers: [
+                            "Host": "localhost:5000",
+                            "User-Agent": "curl/7.54.0",
+                            "Accept": "*/*"
+                        ]
+                )
+
+                let notFound = HttpResponse(
+                        version: "HTTP/1.1",
+                        statusCode: 404,
+                        statusPhrase: "NotFound",
+                        headers: [:]
+                )
+
+                let response = router.checkRoute(request: validRequest)
+                expect(response).to(equal(notFound))
+            }
+        }
+    }
 }
