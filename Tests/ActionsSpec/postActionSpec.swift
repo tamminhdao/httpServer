9db9dd86428f8f2ed94c18fda18802936ff399a3
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-01-03 17:07:36 -0600

    Post action very similar to Put at this point

---
+++ Tests/ActionsSpec/postActionSpec.swift
@@ -0,0 +1,35 @@
+import Foundation
+import Requests
+import Actions
+import Values
+import Quick
+import Nimble
+
+class PostActionsSpec: QuickSpec {
+    override func spec() {
+        describe ("#PostAction") {
+            var action: PostAction!
+            var dataStorage: DataStorage!
+            var request: HttpRequest!
+
+            beforeEach {
+                dataStorage = DataStorage()
+                action = PostAction(dataStorage: dataStorage)
+                request = HttpRequest(
+                        method: HttpMethod.post,
+                        url: "/form",
+                        version: "HTTP/1.1",
+                        headers: [:],
+                        body: ["Content":"Text", "My":"Value"]
+                )
+            }
+
+            it ("can perform a post action") {
+                action.execute(request: request)
+                let allValues = dataStorage.logValues()
+                let expectedValues = ["Content":"Text", "My":"Value"]
+                expect(allValues).to(equal(expectedValues))
+            }
+        }
+    }
+}
