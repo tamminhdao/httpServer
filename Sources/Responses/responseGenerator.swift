7abac11aaf4beb2c8dc7bb6db9fd3d86cfe82b98
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-01-04 17:03:04 -0600

    create responses base on different methods

--- Sources/Responses/responseGenerator.swift
+++ Sources/Responses/responseGenerator.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Requests
 
 public class ResponseGenerator {
     private var errorMessage: String = "<p> URL does not exist </p>"
@@ -7,11 +8,36 @@ public class ResponseGenerator {
     public init() {}
 
 
-    public func generate200Response() -> HttpResponse {
-        return HttpResponse(version: "HTTP/1.1", statusCode: 200, statusPhrase: "OK", headers: ["Content-Length":"0", "Content-Type":"text/html"], body: "")
+    public func generate200Response(method: HttpMethod) -> HttpResponse {
+        switch method {
+            case HttpMethod.get:
+                return HttpResponse(version: "HTTP/1.1",
+                                    statusCode: 200,
+                                    statusPhrase: "OK",
+                                    headers: ["Content-Length":String(getRequestMessage.count), "Content-Type":"text/html"],
+                                    body: getRequestMessage)
+
+            case HttpMethod.post, HttpMethod.put, HttpMethod.head, HttpMethod.delete, HttpMethod.connect, HttpMethod.patch:
+                return HttpResponse(version: "HTTP/1.1",
+                                    statusCode: 200,
+                                    statusPhrase: "OK",
+                                    headers: ["Content-Length":"0", "Content-Type":"text/html"],
+                                    body: "")
+
+            case HttpMethod.options:
+                return HttpResponse(version: "HTTP/1.1",
+                                    statusCode: 200,
+                                    statusPhrase: "OK",
+                                    headers: [:],
+                                    body: "")
+            }
     }
 
     public func generate404Response() -> HttpResponse {
-        return HttpResponse(version: "HTTP/1.1", statusCode: 404, statusPhrase: "NotFound", headers: ["Content-Length":String(errorMessage.count), "Content-Type":"text/html"], body: errorMessage)
+        return HttpResponse(version: "HTTP/1.1",
+                            statusCode: 404,
+                            statusPhrase: "NotFound",
+                            headers: ["Content-Length":String(errorMessage.count), "Content-Type":"text/html"],
+                            body: errorMessage)
     }
 }
