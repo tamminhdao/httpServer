0eaab9cc3929f38a3325babd4f0b39218d6bfa42
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-01-09 13:12:30 -0600

    Perform delete action

--- Sources/Main/main.swift
+++ Sources/Main/main.swift
@@ -16,6 +16,7 @@ let server = Server(parser: httpParser, router: router)
 let nullAction = NullAction()
 let putAction = PutAction(dataStorage: data)
 let postAction = PostAction(dataStorage: data)
+let deleteAction = DeleteAction(dataStorage: data)
 
 routesTable.addRoute(route: Route(url: "/", method: HttpMethod.get, action: nullAction))
 routesTable.addRoute(route: Route(url: "/", method: HttpMethod.head, action: nullAction))
@@ -36,5 +37,6 @@ routesTable.addRoute(route: Route(url: "/method_options", method: HttpMethod.opt
 routesTable.addRoute(route: Route(url: "/method_options", method: HttpMethod.post, action: postAction))
 routesTable.addRoute(route: Route(url: "/method_options2", method: HttpMethod.get, action: nullAction))
 routesTable.addRoute(route: Route(url: "/method_options2", method: HttpMethod.options, action: nullAction))
+
 
 server.run()
---
+++ Tests/ActionsSpec/DeleteActionSpec.swift
@@ -0,0 +1,36 @@
+import Foundation
+import Quick
+import Nimble
+import Actions
+import Values
+import Requests
+
+class DeleteActionSpec: QuickSpec {
+    override func spec() {
+        describe("#DeleteAction") {
+            var action: DeleteAction!
+            var dataStorage: DataStorage!
+            var request: HttpRequest!
+
+            beforeEach {
+                dataStorage = DataStorage()
+                dataStorage.addValues(key: "data", value: "fatcat")
+                action = DeleteAction(dataStorage: dataStorage)
+                request = HttpRequest(
+                        method: HttpMethod.delete,
+                        url: "/form",
+                        version: "HTTP/1.1",
+                        headers: [:],
+                        body: [:]
+                )
+            }
+
+            it ("deletes all data in the data storage") {
+                action.execute(request: request)
+                let allValues = dataStorage.logValues()
+                let expectedValues : [String: String] = [:]
+                expect(allValues).to(equal(expectedValues))
+            }
+        }
+    }
+}
