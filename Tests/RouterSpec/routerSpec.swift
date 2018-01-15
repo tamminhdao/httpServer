b5ada3a50d760c483059f604b98048cdd79dfc2b
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-01-15 14:44:32 -0600

    fix router spec

--- Tests/RouterSpec/routerSpec.swift
+++ Tests/RouterSpec/routerSpec.swift
@@ -19,8 +19,11 @@ class RouterSpec: QuickSpec {
 
             beforeEach {
                 routesTable = RoutesTable()
+                dataStorage = DataStorage()
+                dataStorage.addValues(key: "data", value: "fatcat")
+                responseGenerator = ResponseGenerator(routesTable: routesTable, dataStorage: dataStorage)
                 nullAction = NullAction()
-                router = Router(routesTable: routesTable)
+                router = Router(routesTable: routesTable, responseGenerator: responseGenerator)
                 routesTable.addRoute(route: Route(url: "/", method: HttpMethod.get, action: nullAction))
                 routesTable.addRoute(route: Route(url: "/method_options2", method: HttpMethod.get, action: nullAction))
             }
