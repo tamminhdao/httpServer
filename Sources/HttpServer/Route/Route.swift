9e61e24fd8e38b1651e59ae52a99b0d580b21254
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-02-03 21:40:03 -0600

    Add realm and credentials to routes

--- Sources/HttpServer/Route/Route.swift
+++ Sources/HttpServer/Route/Route.swift
@@ -2,25 +2,26 @@ public class Route {
     public var url: String
     public var method: HttpMethod
     public var action: HttpAction
+    public var realm: String
+    public let credentials: String
 
-//    let routeMap: [String: ()] = [:]
-
-    public init(url: String, method: HttpMethod, action: HttpAction) {
+    public init(url: String, method: HttpMethod, action: HttpAction, realm: String = "", credentials: String = "") {
         self.url = url
         self.method = method
         self.action = action
+        self.realm = realm
+        self.credentials = credentials
     }
-//
-//    func post(){
-//
-//    }
-//
-//    func put() {
-//        returns response
-//    }
-//
-//
-//    func options() {
-//
-//    }
+
+    public func needsAuthorization() -> Bool {
+        return self.realm != "" && self.credentials != ""
+    }
+
+    public func getRealm() -> String {
+        return self.realm
+    }
+
+    public func getCredentials() -> String {
+        return self.credentials
+    }
 }
