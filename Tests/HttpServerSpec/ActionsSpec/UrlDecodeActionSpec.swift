529e632eaf67dfe7576ac469b41a7b4a2d1f1056
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-02-19 13:27:41 -0600

    Passed parameter decode cobspec test

---
+++ Tests/HttpServerSpec/ActionsSpec/UrlDecodeActionSpec.swift
@@ -0,0 +1,45 @@
+import Foundation
+import Quick
+import Nimble
+import HttpServer
+
+class UrlDecodeActionSpec: QuickSpec {
+    override func spec() {
+        describe("#UrlDecodeAction") {
+            var action: UrlDecodeAction!
+            var dataStorage: DataStorage!
+            var request: HttpRequest!
+            var routesTable: RoutesTable!
+
+            beforeEach {
+                dataStorage = DataStorage()
+                routesTable = RoutesTable()
+                action = UrlDecodeAction(routesTable: routesTable, dataStorage: dataStorage)
+                request = HttpRequest(
+                        method: HttpMethod.get,
+                        url: "/params",
+                        params: ["%3C", "this=stuff"],
+                        version: "HTTP/1.1",
+                        headers: [:],
+                        body: [:]
+                )
+            }
+
+            it ("can decode the params of a request") {
+                let response = action.execute(request: request)
+                let expectedResponse = HttpResponse(
+                        statusCode: 200,
+                        statusPhrase: "OK",
+                        headers: ["Content-Length": String(("<this=stuff").count),
+                                  "Content-Type": "text/html",
+                                  "Allow": "",
+                                  "Location": "",
+                                  "WWW-Authenticate": "",
+                                  "Set-Cookie": ""],
+                        body: Data("<this=stuff".utf8))
+                
+                expect(response).to(equal(expectedResponse))
+            }
+        }
+    }
+}
